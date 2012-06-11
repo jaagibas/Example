d2d8b6e81266c7fb4985798e5d2999be9b0f814f
joseba andoni agirre <jaagirre@mondragon.edu>
2012-06-11 11:38:57 +0200

    Sim 5. cambio

--- SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/awt/GameAWT.java
+++ SpaceInvadersDesktopTDD/src/edu/mondragon/eps/jaagirre/game/desktop/tdd/spaceinvaders/awt/GameAWT.java
@@ -12,6 +12,7 @@ import edu.mondragon.eps.jaagirre.game.desktop.tdd.spaceinvaders.GameBoard;
 
 //Sim 1. cambio
 //Sim 2. cambio
+//sim 5 cambio
 public class GameAWT extends Game {
 
 	private JFrame window;
